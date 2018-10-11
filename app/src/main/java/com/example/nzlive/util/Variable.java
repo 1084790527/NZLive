7f4dafb9290c23095a6ee0b01a7f312639642c35
1084790527@qq.c <1084790527@qq.com>
2018-10-11 12:19:27 +0800

    INIT

--- app/src/main/java/com/example/nzlive/util/Variable.java
+++ app/src/main/java/com/example/nzlive/util/Variable.java
@@ -1,7 +1,12 @@
 package com.example.nzlive.util;
 
 public class Variable {
+
+    //服务器IP地址
 //    public static String ServiceIP="http://192.168.152.1:8888/";
     public static String ServiceIP="http://192.168.0.177:8888/";
+
+    //websocket链接地址
     public static String WebsocketUrl="ws://192.168.0.177:8888/websocket";
+
 }
