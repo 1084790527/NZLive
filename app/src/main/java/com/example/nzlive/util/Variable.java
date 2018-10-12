4669902ff59fe5b149652b0d2f75d904a1b94cba
1084790527 <1084790527@qq.com>
2018-10-12 16:57:55 +0800

    INIT

--- app/src/main/java/com/example/nzlive/util/Variable.java
+++ app/src/main/java/com/example/nzlive/util/Variable.java
@@ -4,7 +4,8 @@ public class Variable {
 
     //服务器IP地址
 //    public static String ServiceIP="http://192.168.152.1:8888/";
-    public static String ServiceIP="http://192.168.0.177:8888/";
+    public static String ServiceIP="http://192.168.0.3:8888/";
+//    public static String ServiceIP="http://192.168.0.177:8888/";
 
     //websocket链接地址
     public static String WebsocketUrl="ws://192.168.0.177:8888/websocket";
